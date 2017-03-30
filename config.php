bd2453841b7e7288226bac5e638ddfe821cf6613
Gabriel Darezzo <gabrieldarezzo@yahoo.com.br>
2017-03-30 09:28:37 -0300

    Melhor explicação de como instalar e rodar

--- config.php
+++ config.php
@@ -2,17 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
-function my_autoload ($className) {
-	
+//Quase um jQuery haha
+spl_autoload_register(function($className) {
 	$file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\',DIRECTORY_SEPARATOR, 'class\\' . $className) . ".php";
 	if(file_exists($file)){
 		 require_once($file);
 	}
-
-}
-
-
-
-
-
-spl_autoload_register("my_autoload");
+});
